116a316d9d8777708dec8651b615ba747259537e
meganpekarek <megan@livefront.com>
2025-03-05 10:43:07 -0600

    add headless ui dependency

---
+++ src/TestReactComponent/index.tsx
@@ -0,0 +1,10 @@
+import { OCQuestionnaireFlow } from "optum-perks-web-oc-sdk";
+import { FunctionComponent } from "react";
+
+export const TestReactComponent: FunctionComponent = () => {
+  return (
+    <div>
+      <OCQuestionnaireFlow />
+    </div>
+  );
+};
